8477e62a04fcb2924ad05b48926721f0e3d8d40f
100Gyeon <bjy9966@naver.com>
2020-12-03 23:24:34 +0900

    Create image upload button

---
+++ customButton.js
@@ -0,0 +1,5 @@
+var realBtn = document.getElementById("real_btn");
+var customBtn = document.getElementById("custom_btn");
+customBtn.addEventListener("click", function() {
+    realBtn.click();
+});
