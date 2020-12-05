bd5ca838b009f0e0c4c8886c1f93980752a4ff5e
100Gyeon <bjy9966@naver.com>
2020-12-06 05:26:56 +0900

    Add codes for setting new list based on options

---
+++ pagination/changeList.js
@@ -0,0 +1,13 @@
+function changeList() {
+    var city = document.getElementById("city");
+    var idx = city.selectedIndex;
+    if(city.options[idx].value == "1"){
+        location.href = "/home.html";
+    }
+    else if(city.options[idx].value == "2"){
+        location.href = "page_2.html";
+    }
+    else if(city.options[idx].value == "3"){
+        location.href = "page_3.html";
+    }
+}
