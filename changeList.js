796b09cf236bb29383bfb1bef2d4804e32d615e0
100Gyeon <bjy9966@naver.com>
2020-12-06 20:30:23 +0900

    Refactor codes

--- changeList.js
+++ changeList.js
@@ -1,13 +1,19 @@
 function changeList() {
     var city = document.getElementById("city");
     var idx = city.selectedIndex;
-    if(city.options[idx].value == "1"){
+    if (city.options[idx].value == "1") {
         location.href = "home.html";
     }
-    else if(city.options[idx].value == "2"){
+    else if (city.options[idx].value == "2") {
         location.href = "pagination/page_2.html";
     }
-    else if(city.options[idx].value == "3"){
+    else if (city.options[idx].value == "3") {
         location.href = "pagination/page_3.html";
+    }
+    else if (city.options[idx].value == "4") {
+        location.href = "pagination/page_4.html";
+    }
+    else if (city.options[idx].value == "5") {
+        location.href = "pagination/page_5.html";
     }
 }
--- pagination/changeList.js
+++ pagination/changeList.js
@@ -1,19 +1,19 @@
 function changeList() {
     var city = document.getElementById("city");
     var idx = city.selectedIndex;
-    if(city.options[idx].value == "1"){
+    if (city.options[idx].value == "1") {
         location.href = "/home.html";
     }
-    else if(city.options[idx].value == "2"){
+    else if (city.options[idx].value == "2") {
         location.href = "page_2.html";
     }
-    else if(city.options[idx].value == "3"){
+    else if (city.options[idx].value == "3") {
         location.href = "page_3.html";
     }
-    else if(city.options[idx].value == "4"){
+    else if (city.options[idx].value == "4") {
         location.href = "page_4.html";
     }
-    else if(city.options[idx].value == "5"){
+    else if (city.options[idx].value == "5") {
         location.href = "page_5.html";
     }
 }
