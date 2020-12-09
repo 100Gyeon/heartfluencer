efa4a1e62506bf2ce84b4a196d360ae300d922bc
100Gyeon <bjy9966@naver.com>
2020-12-09 15:44:04 +0900

    Use window.onload

--- popupEffect.js
+++ popupEffect.js
@@ -1,10 +1,13 @@
-var popupForm, closeBtn, hide;
+window.onload = function () {
+    var popupForm, closeBtn, hide;
 
-popupForm = document.querySelector('.popup_form');
-closeBtn = popupForm.querySelector('.close_btn');
-hide = document.querySelector('.hide');
+    popupForm = document.querySelector('.popup_form');
+    closeBtn = popupForm.querySelector('.close_btn');
+    hide = document.querySelector('.hide');
 
-// 팝업을 닫기 위한 버튼 이벤트 리스너
-closeBtn.addEventListener('click', () => {
-    hide.style.display = 'none';
-});
+    // 팝업을 닫기 위한 버튼 이벤트 리스너
+    closeBtn.addEventListener('click', () => {
+        hide.style.display = 'none';
+    });
+
+}
